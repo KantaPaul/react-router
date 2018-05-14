0422c356dda00ca2d4213579184fa36e0ecd9ede
KantaPaul <pobon.paul1994@gmai.com>
2018-05-13 23:46:28 -0700

    router class

--- src/app.js
+++ src/app.js
@@ -1,39 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/style.scss';
-import {BrowserRouter , Route } from 'react-router-dom';
+import Header from './Components/Header';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Help from './Components/Help';
+import NotFound from './Components/NotFound';
+import {BrowserRouter , Route, Switch, Link, NavLink  } from 'react-router-dom';
 
-let Header = () => (
+let HeaderTitle = () => (
   <div>
-    Header
-  </div>
-);
-
-let About = () => (
-  <div>
-    About
-  </div>
-);
-
-let Contact = () => (
-  <div>
-    Contact
-  </div>
-);
-
-let Help = () => (
-  <div>
-    Help
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item" activeClassName="active">
+            <NavLink className="nav-link" exact={true} to="/">Home</NavLink>
+          </li>
+          <li className="nav-item" activeClassName="active">
+            <NavLink className="nav-link" to="/about">About</NavLink>
+          </li>
+          <li className="nav-item" activeClassName="active">
+            <NavLink className="nav-link" to="/contact">Contact</NavLink>
+          </li>
+          <li className="nav-item" activeClassName="active">
+            <NavLink className="nav-link" to="/help">Help</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 );
 
 let routes = (
   <BrowserRouter>
     <div>
-      <Route path="/" component={Header} exact={true}/>
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/help" component={Help} />
+      <HeaderTitle />
+      <Switch>
+        <Route path="/" component={Header} exact={true}/>
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/help" component={Help} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
